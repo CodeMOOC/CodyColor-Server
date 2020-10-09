0c97039d0b761a84a2ef6d11095f65d418979c8d
riccardomaldini <riccardo.maldini@gmail.com>
2020-10-09 16:53:31 +0200

    Switched to version 3.3.4

--- src/codycolor-server/versions.js
+++ src/codycolor-server/versions.js
@@ -2,6 +2,6 @@
  * versions.js
  */
 (function () {
-    module.exports.requiredClient = '3.3.3';
-    module.exports.requiredWall = '3.3.3';
+    module.exports.requiredClient = '3.3.4';
+    module.exports.requiredWall = '3.3.4';
 }());
